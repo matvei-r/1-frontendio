122e27043ed563e8c47ea0d1fd086d8284dbb605
matvei-r <matthewvat@gmail.com>
2023-07-09 23:43:54 -0400

    final commit

--- main.js
+++ main.js
@@ -1,31 +1,26 @@
 const buttonArr = document.querySelectorAll(".rate-button")
 const rating = document.querySelector("#final-rating")
 
-const state1 = document.querySelector("#rate-state")
-const state2 = document.querySelector("#thankyou-state")
-
-let lastClicked; 
-
 buttonArr.forEach( elem  => {
 
     elem.addEventListener("click", () => {
-        lastClicked = elem.innerHTML 
-        rating.innerHTML = lastClicked
+
+        buttonArr.forEach( elem  => {elem.classList.remove("active")})
+
+        rating.innerHTML = elem.innerHTML 
+        elem.classList.add("active")
     })
+})      
 
-})  
-
-
-
+const state1 = document.querySelector("#rate-state")
+const state2 = document.querySelector("#thankyou-state")
 const submitButton = document.querySelector("#submit")
+
 submitButton.addEventListener("click", () => { 
 
     if (rating.innerHTML){
         state1.classList.add("hidden")
         state2.classList.remove("hidden")
     }
-        
-    
-    
 })
 
